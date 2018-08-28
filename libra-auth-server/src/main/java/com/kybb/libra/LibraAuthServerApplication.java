5a941b63854383f65245d772337786992656fd6f
vicykie <122811575@qq.com>
2018-08-28 20:27:14 +0800

    ~~~~

--- libra-auth-server/src/main/java/com/kybb/libra/LibraAuthServerApplication.java
+++ libra-auth-server/src/main/java/com/kybb/libra/LibraAuthServerApplication.java
@@ -1,16 +1,24 @@
 package com.kybb.libra;
 
+import feign.Request;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages={"com.kybb"})
-@EnableFeignClients(basePackages={"com.kybb.**"})
+@SpringBootApplication(scanBasePackages = {"com.kybb"})
+@EnableFeignClients(basePackages = {"com.kybb.**"})
 @ComponentScan(basePackages = {"com.kybb"})
-@EnableHystrix
+//@EnableHystrix
 public class LibraAuthServerApplication {
+
+    @Bean
+    Request.Options feignOptions() {
+        return new Request.Options(10 * 1000, 10 * 1000);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(LibraAuthServerApplication.class, args);
