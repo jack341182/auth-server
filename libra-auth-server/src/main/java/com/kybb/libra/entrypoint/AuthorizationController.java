396a0ea93bf4f9260f441d48124e1655a36bf9fa
vicykie <122811575@qq.com>
2018-08-30 10:23:52 +0800

    ~~~~

--- libra-auth-server/src/main/java/com/kybb/libra/entrypoint/AuthorizationController.java
+++ libra-auth-server/src/main/java/com/kybb/libra/entrypoint/AuthorizationController.java
@@ -1,5 +1,6 @@
 package com.kybb.libra.entrypoint;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class AuthorizationController {
     @Autowired
     @Qualifier("consumerTokenServices")
@@ -15,6 +17,8 @@ public class AuthorizationController {
     @PutMapping("/token/evict")
     public ResponseEntity evictToken(@RequestParam("token") String token) {
         boolean b = consumerTokenServices.revokeToken(token);
+
+        log.info("=====>清除 token  "  +token);
         return ResponseEntity.ok(b);
     }
 }
