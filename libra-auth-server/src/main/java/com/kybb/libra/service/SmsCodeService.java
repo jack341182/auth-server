ea2fd84e8159128ea63579d7665e477017846d26
vicykie <122811575@qq.com>
2018-08-31 10:26:54 +0800

    ~~~~

--- libra-auth-server/src/main/java/com/kybb/libra/service/SmsCodeService.java
+++ libra-auth-server/src/main/java/com/kybb/libra/service/SmsCodeService.java
@@ -1,7 +1,11 @@
 package com.kybb.libra.service;
 
+import com.kybb.columba.message.enums.MessageBusinessEnum;
+import com.kybb.columba.message.request.MessageRequest;
+import com.kybb.columba.message.vo.SmsCaptchaVO;
 import com.kybb.common.cloud.integration.SmsCodeLogin;
 import com.kybb.common.cloud.integration.SmsCodeRequest;
+import com.kybb.common.http.Body;
 import com.kybb.libra.bean.SmsCodeStatus;
 import com.kybb.libra.feign.MessageFeignClient;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +13,8 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -39,29 +45,36 @@ public class SmsCodeService {
      * @return
      */
     public SmsCodeStatus saveAndSendCode(SmsCodeLogin smsCodeLogin) {
-        // TODO: 2018/8/22  发送短信
-//        ResponseEntity<Body<SmsCaptchaVO>> bodyResponseEntity = messageFeignClient.sendCaptcha(MessageRequest.builder()
-//                .mobile(smsCodeRequest.getMobile())
-//                .business(MessageBusinessEnum.LOGIN_CAPTCHA)
-//                .build());
-//        if (bodyResponseEntity.getStatusCode().value() >= HttpStatus.OK.value() && bodyResponseEntity.getStatusCode().value() < 300) {
-//
-//            SmsCaptchaVO code = bodyResponseEntity.getBody().getData();
-//
-//        }
-        // TODO: 2018/8/31 检查缓存的验证码
+
+        // 检查缓存的验证码
         String cacheCode = getCode(smsCodeLogin);
-        if(StringUtils.isEmpty(cacheCode)){
-            String code = "123456";
-            String md5Hex = DigestUtils.md5Hex(code);
-            String encode = passwordEncoder.encode(md5Hex);
-            redisTemplate.opsForValue().set(SMS_CODE_PREFIX + smsCodeLogin.getMobile() + smsCodeLogin.getDeviceId(),
-                    encode, 10, TimeUnit.MINUTES);
+        if (StringUtils.isEmpty(cacheCode)) {
+            //  发送短信
+            ResponseEntity<Body<SmsCaptchaVO>> bodyResponseEntity = messageFeignClient.sendCaptcha(MessageRequest.builder()
+                    .mobile(smsCodeLogin.getMobile())
+                    .business(MessageBusinessEnum.LOGIN_CAPTCHA)
+                    .build());
+            if (bodyResponseEntity.getStatusCode().value() >= HttpStatus.OK.value() && bodyResponseEntity.getStatusCode().value() < 300) {
+
+                SmsCaptchaVO captchaVO = bodyResponseEntity.getBody().getData();
+                String code = captchaVO.getValidateCode();
+                String md5Hex = DigestUtils.md5Hex(code);
+                String encode = passwordEncoder.encode(md5Hex);
+                redisTemplate.opsForValue().set(SMS_CODE_PREFIX + smsCodeLogin.getMobile() + smsCodeLogin.getDeviceId(),
+                        encode, 5, TimeUnit.MINUTES);
+                if (log.isDebugEnabled()) {
+                    log.debug(" mobile " + smsCodeLogin.getMobile() + " code " + code);
+                }
+                return SmsCodeStatus.builder()
+                        .success(true)
+                        .message("验证码发送成功")
+                        .build();
+            }
             return SmsCodeStatus.builder()
-                    .success(true)
-                    .message("验证码发送成功")
+                    .success(false)
+                    .message(bodyResponseEntity.getBody().getMessage())
                     .build();
-        }else {
+        } else {
             return SmsCodeStatus.builder()
                     .success(false)
                     .message("验证码未失效，请检查手机短信")
