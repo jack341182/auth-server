2d48737b207e2feb1460f58ca55dd122504b6946
vicykie <122811575@qq.com>
2018-09-13 15:10:44 +0800

    重构部分登录逻辑

--- libra-auth-server/src/main/java/com/kybb/libra/entrypoint/EncryptController.java
+++ libra-auth-server/src/main/java/com/kybb/libra/entrypoint/EncryptController.java
@@ -25,21 +25,11 @@ public class EncryptController {
 
     @PostMapping("/code")
     public String encrypt(@RequestBody String input) {
-
-        System.out.println(passwordEncoder.matches(input,
-                "$2a$10$hKAeCsPP6K.2OCd7ijvoT.CVKpYWroColGLYacEmCbf63BwhMflZ2"));
         String encode = passwordEncoder.encode(input);
         if (log.isDebugEnabled()) {
             log.debug("===  input str is " + input);
             log.debug("===  encode  str is " + encode);
-
-//            e10adc3949ba59abbe56e057f20f883e
-//            $2a$10$AmyDiCqFkRY4wsUOuNutx.lWZCSYgpWwM/7taJAXc9MTAAEOfrFP6
         }
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode(input));
-        System.out.println(encoder.matches(input,
-                "$2a$10$2noOSC9xPmbwkGMMmy1J/uhmXhTPXuySRRexJVTrrYOIJaEDWXcBm"));
         return encode;
     }
 
